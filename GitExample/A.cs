f32bfa2eecc8b04960c6885a5aa40e23f3f4b2b1
Gesko927 <gesko927@gmail.com>
2017-04-21 19:53:04 +0300

    First Commit

---
+++ GitExample/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExample
+{
+    class A
+    {
+        private string name;
+        private double number;
+        private int count;
+        public A()
+        {
+            name = "A";
+            number = 1.0;
+            count = 1;
+        }
+    }
+}
---
+++ GitExample/B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExample
+{
+    class B
+    {
+        private string name;
+        private double number2;
+        private int count;
+        public B()
+        {
+            name = "B";
+            number2 = 1.0;
+            count = 1;
+        }
+
+    }
+}
